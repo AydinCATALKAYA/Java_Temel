66390cc397ab0e02f06473c371d41dd2b2cdfb54
AYDIN ÇATALKAYA <147395196+AydinCATALKAYA@users.noreply.github.com>
2023-12-22 14:11:58 +0300

    Main.java

---
+++ Mayin_Tarlasi/Main.java
@@ -0,0 +1,3 @@
+ MineSweeper2 game = new MineSweeper2(3,3);//oyun tahtasının boyutunu kendinize gore belirleyebilirsiniz
+        System.out.println("---> Oyunuma Hosgeldiniz <---");
+        game.run();
