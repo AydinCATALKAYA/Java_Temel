0e23e8f09bf36528734d8bf4db8eb32911535f2d
AYDIN ÇATALKAYA <147395196+AydinCATALKAYA@users.noreply.github.com>
2024-01-02 15:59:32 +0300

    DeseneGöreMetot.java

---
+++ DeseneGöreMetot.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class DeseneGöreMetot {
+    public static void  islemyap(int sayi){
+        System.out.print(sayi + " ");
+
+        if(sayi<= 0 ){
+            return;
+        }
+        islemyap(sayi-5);
+
+        if(sayi> 0 ){
+
+            System.out.print(sayi + " ");
+
+        }
+
+    }
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz : ");
+        int n = input.nextInt();
+
+        islemyap(n);
+
+    }
+}
